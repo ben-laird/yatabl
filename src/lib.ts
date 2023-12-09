8bf57d74fd9c1b189481d5d0225f71f7ac83994a
Benjamin Laird <86321957+ben-laird@users.noreply.github.com>
2023-12-09 01:20:31 -0500

    feat: polish types, add docs

--- src/lib.ts
+++ src/lib.ts
@@ -12,6 +12,8 @@ export * as y from "./lib.ts";
  */
 const yatabl_tag = Symbol("yatabl/tag");
 
+type Switch<T, U = unknown> = unknown extends U ? T : U;
+
 /**
  * Internal class to manage YATABL's tag logic. Do not use!
  */
@@ -78,7 +80,6 @@ namespace $P {
 //#region Types
 
 export type DataStructure =
-  | Record<string, unknown>
   | Record<string | number | symbol, unknown>
   | unknown[];
 
@@ -111,10 +112,10 @@ export type Container<T> = { value: T };
 
 // tag
 
-type Switch<T, U = unknown> = unknown extends U ? T : U;
-
 /**
- * _Overload 1: Yatable only, with specified input type_
+ * Tag something by using a Yatable as a blank factory function
+ * 
+ * _Overload 1: Yatable only_
  * @param yatable
  */
 export function tag<T extends DataStructure, U>(
@@ -122,13 +123,17 @@ export function tag<T extends DataStructure, U>(
 ): Yatable<Tag.Anon<T>, Switch<T, U>>;
 
 /**
+ * Tag something directly with no identifier
+ * 
  * _Overload 2: Thing only_
  * @param thing
  */
 export function tag<T extends DataStructure>(thing: T): Tag.Anon<T>;
 
 /**
- * _Overload 3: Identifier and yatable, with specified input type_
+ * Tag something by using a Yatable as a factory function with an identifier
+ * 
+ * _Overload 3: Identifier and yatable_
  * @param identifier
  * @param yatable
  */
@@ -138,6 +143,8 @@ export function tag<const I extends Tag.Id, T extends DataStructure, U>(
 ): Yatable<Tag<I, T>, Switch<T, U>>;
 
 /**
+ * Tag something directly with an identifier
+ * 
  * _Overload 4: Identifier and thing_
  * @param identifier
  * @param thing
@@ -186,6 +193,7 @@ export function tag<const I extends Tag.Id, T extends DataStructure, X>(
 
 /**
  * Escape hatch used to remove tags and revert back to the structural type system
+ * 
  * @param tagged thing to untag
  * @returns untagged thing
  */
@@ -193,6 +201,12 @@ export function untag<T extends DataStructure>(tagged: Tag<Tag.Name, T>) {
   return tagged[yatabl_tag].thing;
 }
 
+/**
+ * Get the identifier that this thing was tagged with
+ * 
+ * @param tagged Any tagged thing
+ * @returns identifier of tag
+ */
 export function getIdentifier<N extends Tag.Name>(
   tagged: Tag<N, DataStructure>
 ) {
@@ -201,12 +215,23 @@ export function getIdentifier<N extends Tag.Name>(
 
 // isTagged
 
+/**
+ * Type guard to determine whether something is tagged, without regard for an identifier
+ * 
+ * @param thing thing to test
+ */
 export function isTagged<T extends DataStructure>(
-  thing: T | Tag<Tag.Name, T>
+  thing: Tag<Tag.Name, T> | T
 ): thing is Tag<Tag.Name, T>;
+/**
+ * Type guard to determine whether something is tagged with a given identifier
+ * 
+ * @param identifier identifier to test against
+ * @param thing thing to test
+ */
 export function isTagged<const I extends Tag.Id, T extends DataStructure>(
   identifier: I,
-  thing: T
+  thing: Tag<I, T> | T
 ): thing is Tag<I, T>;
 /**
  * Internal implementation
@@ -232,6 +257,13 @@ export function yatable<T extends DataStructure>(): Yatable<T> {
   return (thing: T) => thing;
 }
 
+/**
+ * Utility function to allow `yatabl` to work with any type
+ * by wrapping it in a container for it to satisfy the `extends DataStructure` constraint
+ * 
+ * @param value value to put in the container
+ * @returns value wrapped in a container
+ */
 export function container<T>(value: T): Container<T> {
   return { value };
 }
